b89ac6de3933311b5aad28d2c4269a1b0d3d2393
fbehddn <tom0611@naver.com>
2023-03-31 00:21:08 +0900

    [11718 -> Bronze 5] 그대로 출력하기

---
+++ Bronze5/baekjoon_10757/Main.java
@@ -0,0 +1,14 @@
+package Bronze5.baekjoon_10757;
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long long a = sc.nextLong();
+        long long b = sc.nextLong();
+
+    }
+}
+*/
---
+++ Bronze5/baekjoon_11718/Main.java
@@ -0,0 +1,12 @@
+package Bronze5.baekjoon_11718;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
