881c8585c9e8012a63a1ad0cc0fe04e3d065f0f3
fbehddn <tom0611@naver.com>
2023-03-31 08:49:17 +0900

    [2743 -> Bronze 5] 단어 길이 재기

---
+++ Bronze5/baekjoon_2743/Main.java
@@ -0,0 +1,12 @@
+package Bronze5.baekjoon_2743;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.next().length();
+        System.out.println(a);
+        sc.close();
+    }
+}
