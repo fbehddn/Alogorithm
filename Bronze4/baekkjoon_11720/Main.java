114492ddf6f86ff3d96d59f9f58f4c5b8e961fe7
fbehddn <tom0611@naver.com>
2023-03-31 16:12:55 +0900

    [11720 -> Bronze 4] 숫자의 합

---
+++ Bronze4/baekkjoon_11720/Main.java
@@ -0,0 +1,20 @@
+package Bronze4.baekkjoon_11720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=sc.nextInt();
+        String str=sc.next();
+
+        sc.close();
+
+        int sum=0;
+
+        for (int i = 0; i < count; i++) {
+            sum+=str.charAt(i)-'0';
+        }
+        System.out.println(sum);
+    }
+}
