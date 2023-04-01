22c17c6e049a6dc5a10d1960923fe78a44f1984c
fbehddn <tom0611@naver.com>
2023-04-01 18:13:18 +0900

    [1009 -> Bronze 2] 분산처리

---
+++ Bronze2/Baekjoon_1009/Main.java
@@ -0,0 +1,21 @@
+package Bronze2.Baekjoon_1009;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int a,b,r;
+
+        for (int i = 0; i < N; i++) {
+            a=sc.nextInt();
+            b=sc.nextInt();
+            r=1;
+            for (int j = 0; j < b; j++) r=(a*r)%10;
+            if(r==0) r=10;
+            System.out.println(r);
+        }
+        sc.close();
+    }
+}
