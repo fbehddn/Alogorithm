3fc65080efb64ce9bd1e62eba839d7849081f1dc
fbehddn <tom0611@naver.com>
2023-04-01 16:38:09 +0900

    [2525 -> Bronze 3] 오븐 시계

---
+++ Bronze3/baekjoon_2525/Main.java
@@ -0,0 +1,30 @@
+package Bronze3.baekjoon_2525;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour,min,addMin;
+
+        hour=sc.nextInt();
+        min=sc.nextInt();
+        addMin=sc.nextInt();
+
+        min+=addMin;
+
+        while(min>=60){
+            int r1= min/60;
+            hour+=r1;
+            if(hour>=24){
+                int r3=hour%24;
+                hour=0;
+                hour+=r3;
+                int r2= hour/24;
+                min%=60;
+            }
+            min%=60;
+        }
+        System.out.print(hour+" "+min);
+    }
+}
