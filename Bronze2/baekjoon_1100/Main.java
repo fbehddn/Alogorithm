c71be1c0bafc8cbc2f0b3596d19b47601b5f6735
fbehddn <tom0611@naver.com>
2023-04-01 22:42:53 +0900

    [1100 -> Bronze 2] 하얀 칸

---
+++ Bronze2/baekjoon_1100/Main.java
@@ -0,0 +1,21 @@
+package Bronze2.baekjoon_1100;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count =0;
+        String s = "";
+        for(int i=0; i<8; i++){
+            s=br.readLine();
+            for (int j = 0; j < 8; j++) {
+                if(s.charAt(j)=='F' && (i+j)%2==0) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
