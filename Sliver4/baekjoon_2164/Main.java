c9721d7917773bf2ffa2beca20d97eef415afd24
fbehddn <tom0611@naver.com>
2024-02-08 14:14:15 +0900

    [2164 -> Silver 4 ] 카드2 리팩토링 (자료 구조 - 큐)

--- Sliver4/baekjoon_2164/Main.java
+++ Sliver4/baekjoon_2164/Main.java
@@ -1,35 +1,42 @@
 package Sliver4.baekjoon_2164;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        int[] q = new int[2 * n];
+        Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 1; i <= n; i++) {
-            q[i] = i;
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
         }
 
-        int pre_ind = 1;
-        int last_ind = n;
-
-        while (n-- > 1) {
-            pre_ind++;
-            q[last_ind+1] = q[pre_ind];
-            last_ind++;
-            pre_ind++;
-
+        while (queue.size()!=1) {
+            queue.remove();
+            Integer findNumber = queue.remove();
+            queue.add(findNumber);
         }
-        System.out.println(q[pre_ind]);
+        System.out.println(queue.peek());
     }
 }
 /**
- * 1 2 3 4
- * 1 2 3 4 2
- * 1 2 3 4 2 3
+ * 1 2 3 4 5 6
+ *
+ * 2 3 4 5 6
+ * 3 4 5 6 2
+ *
+ * 4 5 6 2
+ * 5 6 2 4
+ *
+ * 6 2 4
+ * 2 4 6
+ *
+ * 4 6
+ * 6 4
+ *
+ * 4
  */
