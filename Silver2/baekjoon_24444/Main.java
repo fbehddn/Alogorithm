689820860b9e9ae2c6475f2c63f5b53328fc4bef
fbehddn <tom0611@naver.com>
2024-02-11 23:54:22 +0900

    [24444 -> Silver 2 ] 알고리즘 수업 - 너비 우선 탐색 2 이게왜안돼 (자료 구조 - BFS)

--- Silver2/baekjoon_24444/Main.java
+++ Silver2/baekjoon_24444/Main.java
@@ -3,13 +3,13 @@ package Silver2.baekjoon_24444;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static boolean visited[];
-    static Queue<Integer> queue;
+    static ArrayList<Integer>[] A;
+    static int result[];
+    static int cnt = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,27 +19,48 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N];
-        queue = new LinkedList<>();
+        visited = new boolean[N+1];
+        result = new int[N];
+        A = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            A[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-
-
+            A[s].add(e);
+            A[e].add(s);
         }
 
-        visited[R] = true;
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(A[i]);
+        }
 
         bfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(result[i] + "\n");
+        }
     }
 
-    private static void bfs(int n) {
-        for (int i : queue) {
-            if(visited[i]) return;
-            visited[i] = true;
-            queue.add(n);
+    private static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+        result[start] = cnt++;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : A[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    result[next]++;
+                    q.add(next);
+                }
+            }
         }
     }
 }
