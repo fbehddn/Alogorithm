bdc2c87e06ea059581f6cd6e501dd413580e532e
fbehddn <tom0611@naver.com>
2024-07-02 14:03:01 +0900

    [25314 -> Bronze 5 ] 코딩은 체육과목 입니다 (반복문)

---
+++ Bronze5/baekjoon25314/Main.java
@@ -0,0 +1,18 @@
+package Bronze5.baekjoon25314;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ans = "";
+        int n = sc.nextInt() / 4;
+
+        for(int i=0; i<n; i++) {
+            ans += "long ";
+        }
+        ans += "int";
+
+        System.out.println(ans);
+    }
+}
