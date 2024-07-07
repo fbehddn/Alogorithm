6b751758ee4f94b4769a17068fea8d89104ee72c
fbehddn <tom0611@naver.com>
2024-07-07 22:47:12 +0900

    [11654 -> Bronze 5 ] 아스키 코드 (문자열)

---
+++ Bronze5/baekjoon_11654/Main.java
@@ -0,0 +1,10 @@
+package Bronze5.baekjoon_11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) sc.next().charAt(0));
+    }
+}
