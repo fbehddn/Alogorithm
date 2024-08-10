3984fe39cab506e41d9b2d0447d5b39fe371f2a0
fbehddn <tom0611@naver.com>
2024-08-10 15:30:56 +0900

    [14425 -> Silver4] 문자열 집합 - 집합과 맵 (리팩토링)

--- Sliver4/baekjoon_14425/Main.java
+++ Sliver4/baekjoon_14425/Main.java
@@ -7,8 +7,7 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    static HashMap<Integer, String > map = new HashMap<>();
-    static String[] strArr;
+    static HashMap<String, Integer > map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,23 +15,15 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        strArr = new String[M];
-        int cnt = 0;
         for (int i = 0; i < N; i++) {
-            map.put(cnt++, br.readLine());
+            map.put(br.readLine(), 0);
         }
-        for (int i = 0; i < M; i++) {
-            strArr[i] = br.readLine();
-        }
+        int cnt = 0;
 
-        int result = 0;
         for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if(map.get(j).equals(strArr[i])) {
-                    result++;
-                }
-            }
+            if(map.containsKey(br.readLine()))
+                cnt++;
         }
-        System.out.println(result);
+        System.out.println(cnt);
     }
 }
