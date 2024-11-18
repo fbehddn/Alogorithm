4d456cfc457706b6a13ba26fca6f31544dc2d4a1
fbehddn <tom0611@naver.com>
2024-11-18 15:49:46 +0900

    [2667 -> Silver1] 단지 번호 붙이기(그래프)

--- Silver1/baekjoon_2667/Main.java
+++ Silver1/baekjoon_2667/Main.java
@@ -4,83 +4,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
-    static int[][] arr;
-    static int N;
-    static int count = 0;
-    static int count1 = 0;
-    static ArrayList<Integer> result = new ArrayList<>();
+    static int N, eachHome;
+    static List<Integer> houseCount;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
+        map = new int[N + 1][N + 1];
         visited = new boolean[N + 1][N + 1];
-        arr = new int[N + 1][N + 1];
+        houseCount = new ArrayList<>();
+        eachHome = 1;
 
+        // 지도 초기화
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+            String str = br.readLine();
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(line.substring(j, j + 1));
-            }
-        }
-        int size = N * N;
-        int sum = 0;
-        /**
-         * 총 1의 개수 찾기 -> 0 에서 다음으로 탐색이 안됨
-         * 방문은 노드값 1을 0으로 바꿈 -> 0 에서 다음으로 탐색이 안됨
-         *
-         *
-         */
-
-        int numbersOfOne = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(arr[i][j] == 1 ) numbersOfOne++;
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
-        while (count1 < numbersOfOne) {
-            count = 0;
-            int semiResult = BFS(0, 0);
-            result.add(semiResult);
-            sum++;
-        }
-
-        System.out.println(sum);
-        for (Integer integer : result) {
-            System.out.println(integer);
-        }
-    }
-
-    private static int BFS(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-            visited[x][y] = true;
-            for (int i = 0; i < 4; i++) {
-                int nextX = now[0] + dx[i];
-                int nextY = now[1] + dy[i];
-                if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < N) {
-                    if (arr[nextX][nextY] != 0 && !visited[nextX][nextY]) {
-//                        visited[nextX][nextY] = true;
-                        arr[nextX][nextY] = 0;
-                        count1++;
-                        count++;
-                        q.add(new int[]{nextX, nextY});
-                    }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    eachHome = 1;
+                    dfs(i, j);
+                    houseCount.add(eachHome);
                 }
             }
         }
-        return count;
+
+        Collections.sort(houseCount);
+
+        System.out.println(houseCount.size());
+        for (Integer i : houseCount) {
+            System.out.println(i);
+        }
+    }
+
+    private static void dfs(int i, int j) {
+        visited[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int nextX = i + dx[k];
+            int nextY = j + dy[k];
+
+            if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < N && !visited[nextX][nextY] && map[nextX][nextY]==1) {
+                eachHome++;
+                dfs(nextX, nextY);
+            }
+        }
     }
 }
