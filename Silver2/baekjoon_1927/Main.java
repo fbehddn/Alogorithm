8ed54bc797911075a8a36f5c82f7c68db8a9fc40
fbehddn <tom0611@naver.com>
2024-11-26 15:40:21 +0900

    [1927 -> Silver2] 최소 힙(우선순위 큐)

---
+++ Silver2/baekjoon_1927/Main.java
@@ -0,0 +1,29 @@
+package Silver2.baekjoon_1927;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                if (queue.size() == 0) {
+                    sb.append(num).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
