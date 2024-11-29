308ea0e60050d06aff2d1be752b37e44ceca1ff1
fbehddn <tom0611@naver.com>
2024-11-29 22:57:18 +0900

    [1504 -> Gold4] 특정한 최단 경로(다익스트라)

---
+++ Gold4/baekjoon_1504/Main.java
@@ -0,0 +1,102 @@
+package Gold4.baekjoon_1504;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, E;
+    static ArrayList<Node>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b, weight));
+            graph[b].add(new Node(a, weight));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
+
+        int from1ToV1 = dijkstra(1, v1);
+        int fromV1ToV2 = dijkstra(v1, v2);
+        int fromV2ToN = dijkstra(v2, N);
+
+        int from1ToV2 = dijkstra(1, v2);
+        int fromV2ToV1 = dijkstra(v2, v1);
+        int fromV1ToN = dijkstra(v1, N);
+
+        int route1 = (from1ToV1 == Integer.MAX_VALUE || fromV1ToV2 == Integer.MAX_VALUE || fromV2ToN == Integer.MAX_VALUE)
+                ? Integer.MAX_VALUE
+                : from1ToV1 + fromV1ToV2 + fromV2ToN;
+
+        int route2 = (from1ToV2 == Integer.MAX_VALUE || fromV2ToV1 == Integer.MAX_VALUE || fromV1ToN == Integer.MAX_VALUE)
+                ? Integer.MAX_VALUE
+                : from1ToV2 + fromV2ToV1 + fromV1ToN;
+
+        int minimumDistance = Math.min(route1, route2);
+
+        System.out.println(minimumDistance == Integer.MAX_VALUE ? -1 : minimumDistance);
+    }
+
+    static int dijkstra(int start, int end) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        int[] d = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            d[i] = Integer.MAX_VALUE;
+        }
+        queue.add(new Node(start, 0));
+        d[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            int node = cur.node;
+
+            for (Node temp : graph[node]) {
+                int nextNode = temp.node;
+                int distance = temp.weight;
+
+                if (d[nextNode] > d[node] + distance) {
+                    d[nextNode] = d[node] + distance;
+                    queue.add(new Node(nextNode, d[nextNode]));
+                }
+            }
+        }
+
+        return d[end];
+    }
+
+    static class Node implements Comparable<Node> {
+        int node;
+        int weight;
+
+        public Node(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+}
