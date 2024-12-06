bba52be040e42c554ff00b59480a8e3cdaedf8dc
fbehddn <tom0611@naver.com>
2024-12-06 13:43:58 +0900

    [7576 -> Gold5] 토마토 (그래프와 순회) - 리팩토링

--- gold5/baekjoon_7576/Main.java
+++ gold5/baekjoon_7576/Main.java
@@ -10,64 +10,55 @@ public class Main {
     static int[][] map;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
-    static List<int[]> startIdx;
+    static Queue<int[]> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        M = Integer.parseInt(st.nextToken()); //열
-        N = Integer.parseInt(st.nextToken()); //행
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         map = new int[N][M];
-        startIdx = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 1) {
-                    startIdx.add(new int[]{i, j});
-
+                    queue.add(new int[]{i, j});
                 }
             }
         }
 
-        int result = bfs();
+        bfs();
 
+        int result = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (map[i][j] == 0) {
-                    result = -1;
+                    System.out.println(-1);
+                    return;
                 }
+                result = Math.max(result, map[i][j]);
             }
         }
-        System.out.println(result);
+        System.out.println(result - 1);
     }
 
-    private static int bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < startIdx.size(); i++) {
-            queue.add(new int[]{startIdx.get(i)[0], startIdx.get(i)[1]});
-        }
-
-        int day = 0;
-
+    private static void bfs() {
         while (!queue.isEmpty()) {
-            int size = queue.size(); // 큐 크기 고정
+            int size = queue.size();
             for (int t = 0; t < size; t++) {
                 int[] current = queue.poll();
                 for (int i = 0; i < 4; i++) {
-                    int nextX = current[1] + dx[i];
-                    int nextY = current[0] + dy[i];
-                    if (0 <= nextX && nextX < M && 0 <= nextY && nextY < N && map[nextY][nextX] == 0) {
-                        queue.add(new int[]{nextY, nextX});
-                        map[nextY][nextX] = 1;
+                    int nextRow = current[0] + dx[i];
+                    int nextCol = current[1] + dy[i];
+                    if (0 <= nextRow && nextRow < N && 0 <= nextCol && nextCol < M && map[nextRow][nextCol] == 0) {
+                        queue.add(new int[]{nextRow, nextCol});
+                        map[nextRow][nextCol] = map[current[0]][current[1]] + 1;
                     }
                 }
             }
-            day++;
         }
-        return day - 1;
     }
 }
