b0ec857aec3e9393a40b426220b9a83292b38c4c
fbehddn <tom0611@naver.com>
2025-03-31 17:18:45 +0900

    [5430 -> gold5] AC - 구현,덱 - 리팩토링(시간단축 3배)

--- gold5/baekjoon_5430/Main.java
+++ gold5/baekjoon_5430/Main.java
@@ -3,79 +3,65 @@ package gold5.baekjoon_5430;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder result = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder result = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		ArrayDeque<Integer> deque;
+		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 
-		String str;
-		int size;
-		List<Integer> list;
-
 		while (T-- > 0) {
-			str = br.readLine();
-			size = Integer.parseInt(br.readLine());
-			list = new ArrayList<>();
-
-			String original = br.readLine();
-			String replace = original.replace("[", "");
-			String res = replace.replace("]", "");
-			String[] strs = res.split(",");
+			String command = br.readLine();
+			int size = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), "[],");
+			deque = new ArrayDeque<>();
 
 			for (int i = 0; i < size; i++) {
-				list.add(Integer.parseInt(strs[i]));
+				deque.add(Integer.parseInt(st.nextToken()));
 			}
-
-			boolean direction = true; // true -> 머리 가리키고 있음
-			StringBuilder sb = new StringBuilder();
-			sb.append('[');
-			for (int i = 0; i < str.length(); i++) {
-				switch (str.charAt(i)) {
-					case 'R': {
-						if(direction) {
-							direction = false;
-						}else{
-							direction = true;
-						}
-						break;
-					}
-					case 'D': {
-						if (list.isEmpty()) {
-							sb = new StringBuilder();
-							sb.append("error");
-							break;
-						} else {
-							if (direction) {
-								list.remove(0);
-								break;
-							} else {
-								list.remove(list.size() - 1);
-							}
-							break;
-						}
-					}
-				}
-			}
-			for(int i=0; i<list.size(); i++){
-				if(direction){
-					sb.append(list.get(i));
-				}else
-					sb.append(list.get(list.size() - i - 1));
-				if(i != list.size() - 1){
-					sb.append(",");
-				}
-			}
-			if (!sb.toString().equals("error")) {
-				sb.append(']');
-			}
-			result.append(sb).append("\n");
+			AC(command, deque);
 		}
 		System.out.println(result);
+	}
+
+	private static void AC(String command, ArrayDeque<Integer> deque) {
+		boolean direction = true; // true -> idx:0 가리키고 있음
+		for (char cmd : command.toCharArray()) {
+			if (cmd == 'R') {
+				direction = !direction;
+			} else if (cmd == 'D') {
+				if (deque.isEmpty()) {
+					result.append("error\n");
+					return;
+				}
+				if(direction) {
+					deque.pollFirst();
+				} else deque.pollLast();
+			}
+		}
+		makePrint(deque, direction);
+	}
+
+	private static void makePrint(ArrayDeque<Integer> deque, boolean direction) {
+		result.append("[");
+		if (!deque.isEmpty()) {
+			if (direction) {
+				result.append(deque.pollFirst());
+				while (!deque.isEmpty()) {
+					result.append(',').append(deque.pollFirst());
+				}
+			} else {
+				result.append(deque.pollLast());
+				while (!deque.isEmpty()) {
+					result.append(',').append(deque.pollLast());
+				}
+			}
+		}
+		result.append("]").append("\n");
 	}
 }
