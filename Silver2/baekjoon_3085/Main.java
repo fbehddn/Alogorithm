1bfdbcab9e8a9e620779fa3a35fdf1658dc4ef42
fbehddn <tom0611@naver.com>
2025-04-05 15:16:07 +0900

    [3085 -> silver2] 사탕 게임 - 리팩토링

--- Silver2/baekjoon_3085/Main.java
+++ Silver2/baekjoon_3085/Main.java
@@ -3,21 +3,16 @@ package Silver2.baekjoon_3085;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 	static int N;
 	static char[][] colors;
 	static int result;
-	static Queue<Point> queue;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		colors = new char[N][N];
-		queue = new LinkedList<>();
 
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
@@ -25,82 +20,58 @@ public class Main {
 				colors[i][j] = str.charAt(j);
 			}
 		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (j != N - 1) {
-					if (colors[i][j] != colors[i][j + 1]) {
-						queue.add(new Point(new int[]{i, j}, new int[]{i, j + 1}));
-					}
-				}
-				if (i != 0) {
-					if (colors[i][j] != colors[i - 1][j]) {
-						queue.add(new Point(new int[]{i, j}, new int[]{i - 1, j}));
-					}
-				}
-			}
-		}
+
 		result = 0;
-		swap();
+		function();
 		System.out.println(result);
 	}
 
-	private static void swap() {
-		while (!queue.isEmpty()) {
-			char[][] charArr = new char[N][N];
-			for (int i = 0; i < N; i++) {
-				charArr[i] = Arrays.copyOf(colors[i], N);
+	private static void function() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (j < N - 1) {
+					swap(i, j, i, j + 1);
+					getMaxCount();
+					swap(i, j + 1, i, j);
+
+				}
+				if (i < N - 1) {
+					swap(i, j, i + 1, j);
+					getMaxCount();
+					swap(i + 1, j, i, j);
+				}
 			}
-			Point point = queue.poll();
-			int[] point1 = point.start;
-			int[] point2 = point.end;
-			char temp = charArr[point1[0]][point1[1]];
-			charArr[point1[0]][point1[1]] = charArr[point2[0]][point2[1]];
-			charArr[point2[0]][point2[1]] = temp;
-			result = Math.max(result, getResult(charArr));
 		}
+		result = Math.max(result, getMaxCount());
 	}
 
-	private static int getResult(char[][] arr) {
-		int result = 1;
+	public static void swap(int row1, int col1, int row2, int col2) {
+		char temp = colors[row1][col1];
+		colors[row1][col1] = colors[row2][col2];
+		colors[row2][col2] = temp;
+	}
 
+	private static int getMaxCount() {
+		int maxCount = 1;
 		//row
 		for (int i = 0; i < N; i++) {
 			int cnt = 1;
-			for (int j = 0; j < N; j++) {
-				if (j != N - 1) {
-					if (arr[i][j] == arr[i][j + 1]) cnt++;
-					else {
-						result = Math.max(result, cnt);
-						cnt = 1;
-					}
-				}
+			for (int j = 0; j < N - 1; j++) {
+				if (colors[i][j] == colors[i][j + 1]) cnt++;
+				else cnt = 1;
+				maxCount = Math.max(maxCount, cnt);
 			}
-			result = Math.max(result, cnt);
 		}
 
 		//세로
 		for (int i = 0; i < N; i++) {
 			int cnt = 1;
-			for (int j = 0; j < N; j++) {
-				if (j != N - 1) {
-					if (arr[j][i] == arr[j + 1][i]) cnt++;
-					else {
-						result = Math.max(result, cnt);
-						cnt = 1;
-					}
-				}
+			for (int j = 0; j < N - 1; j++) {
+				if (colors[j][i] == colors[j + 1][i]) cnt++;
+				else cnt = 1;
+				maxCount = Math.max(maxCount, cnt);
 			}
-			result = Math.max(result, cnt);
 		}
-		return result;
-	}
-}
-
-class Point {
-	int[] start, end;
-
-	public Point(int[] start, int[] end) {
-		this.start = start;
-		this.end = end;
+		return maxCount;
 	}
 }
